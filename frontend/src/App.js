f9eec2ea87717982212e8b82d60de60900736c67
Mehdi Elhaij <mehdi.elhaij@hotmail.com>
2017-07-31 04:03:18 +0100

    frontend: basic layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,14 +5,10 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="status-header">
+        <div className="indicator"></div>
+        <div className="title">Inventory Management Demo.</div>
+        <div className="subtitle">Made with ReactJs, NodeJs, MongoDB and Love</div>
       </div>
     );
   }
