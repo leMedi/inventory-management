78af8995e31f29f18a18c87de09311b7757d60b8
Mehdi Elhaij <mehdi.elhaij@hotmail.com>
2017-07-31 12:31:24 +0100

    backend: product API(adding & listing)

--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -1,25 +1,38 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var productRouter = express.Router();
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // Require Item model in our routes module
 var Product = require('../models/Product');
 
 // Defined store route
 productRouter.route('/add/post').post(function (req, res) {
+    console.log(req.body);
   var product = new Product(req.body);
+//   product.title = req.body.title;
+//   product.description = req.body.description;
+//   product.price = req.body.price;
+  console.log(product);
       product.save()
     .then(product => {
-    res.json('Product added successfully');
+        res.json('Product added successfully');
+        console.log('Product added successfully');
+        console.log(product);
     })
     .catch(err => {
-    res.status(400).send("unable to save to database");
+        res.status(400).send("unable to save to database");
+        console.log('unable to save to databasey');
     });
 });
 
 // Defined get data(index or listing) route
 productRouter.route('/').get(function (req, res) {
-  product.find(function (err, itms){
+  Product.find(function (err, itms){
     if(err){
       console.log(err);
     }
@@ -32,7 +45,7 @@ productRouter.route('/').get(function (req, res) {
 // Defined edit route
 productRouter.route('/edit/:id').get(function (req, res) {
   var id = req.params.id;
-  product.findById(id, function (err, product){
+  Product.findById(id, function (err, product){
       res.json(product);
   });
 });
