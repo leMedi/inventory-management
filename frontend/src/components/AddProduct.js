19b6cc8a75a3fbca0788c0cee294c19110e24f32
Mehdi Elhaij <mehdi.elhaij@hotmail.com>
2017-07-31 04:35:08 +0100

    frontend: Product Page

---
+++ frontend/src/components/AddProduct.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+
+class AddProduct extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {value: ''};
+
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+      alert(this.state.value);
+      event.preventDefault();
+    }
+    
+    render() {
+        return (
+            <div>
+                <div className="status-header">
+                    <div className="indicator"></div>
+                    <div className="title">Add New Product.</div>
+                    <div className="subtitle">Fill in the information please.</div>
+                </div>
+
+                <div className="status-uptime">
+                </div>
+
+                <div className="status-list">
+                    <form action="">
+                        
+                        <div className="field">
+                            <label className="label">Title</label>
+                            <div className="control">
+                                <input className="input" type="text" placeholder="Text input"/>
+                            </div>
+                        </div>
+
+                        <div className="field">
+                            <label className="label">Description</label>
+                            <div className="control">
+                                <input className="input" type="text" placeholder="Text input"/>
+                            </div>
+                        </div>
+
+                        <div className="field">
+                            <label className="label">Price</label>
+                            <div className="control">
+                                <input className="input" type="text" placeholder="Text input"/>
+                            </div>
+                        </div>
+
+                        <div class="control">
+                            <button className="button is-primary">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            
+        );
+    }
+}
+
+export default AddProduct;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './App';
+import AddProduct from './components/AddProduct';
+
+ReactDOM.render(
+  <Router>
+      <div>
+        <Route exact path='/' component={App} />
+        <Route path='/add-product' component={AddProduct} />
+      </div>
+  </Router>,
+  document.getElementById('root')
+);
