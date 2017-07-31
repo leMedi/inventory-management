bf75369966c3947bef133d1a6acceeda00884f28
Mehdi Elhaij <mehdi.elhaij@hotmail.com>
2017-07-31 12:30:59 +0100

    backend: Product Schema

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -2,13 +2,12 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 // Define collection and schema for Items
-var Item = new Schema({
-  item: {
-    type: String
-  },
-
+var Product = new Schema({
+    title: String,
+    description: String,
+    price: String
 },{
     collection: 'product'
 });
 
-module.exports = mongoose.model('Product', Item);
+module.exports = mongoose.model('Product', Product);
